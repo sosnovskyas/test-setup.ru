8eb83709370296afe8f70b0ac157d575f0430acd
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-17 14:45:08 +0300

    added content articles

--- src/components/articleList/index.js
+++ src/components/articleList/index.js
@@ -3,7 +3,7 @@ import "./articleList.styl";
 import articleListTemplate from './articleList.jade'
 
 export default class ArticleList {
-  constructor({elem}) {
-    elem.innerHTML = articleListTemplate();
+  constructor({elem, articles}) {
+    elem.innerHTML = articleListTemplate({articles: articles});
   }
 }
