aa8e3c04c890d7c749e7fd7a6bdaad7574a69a34
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-17 13:49:19 +0300

    index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 'use strict';
 
+// common styles
 import './index.styl'
-import tempalte from './index.jade'
+import indexTemplate from './index.jade'
+import Header from './components/header'
+import Carousel from './components/carousel'
 
-import Carousel from './carousel'
+document.body.innerHTML = indexTemplate();
 
-// let root = document.createElement('div');
-document.body.innerHTML = tempalte();
+new Header({
+  elem: document.querySelector('.header'),
+  navItems: [
+    {title: 'home', link: 'index.html'},
+    {title: 'blog', link: 'blog.html'},
+    {title: 'contacts', link: 'contacts.html'}
+  ]
+});
+
+new Carousel({
+  elem: document.querySelector('.carousel'),
+  items: [
+    {title: 'About Us', image: 'A.png', link: 'about.html' , headerBgColor: 'darkGreen'},
+    {title: 'Products', image: 'P.png', link: 'products.html', headerBgColor: 'lightGreen'},
+    {title: 'News',     image: 'N.png', link: 'news.html', headerBgColor: 'brown'}
+
+  ]
+});
